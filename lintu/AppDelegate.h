c18035ac9cf2b9c7fde029e411a498c62acf2318
Frank Prößdorf <fpro@berlin-225.guest.futurice.org>
2013-01-24 11:02:51 +0100

    Initial Commit

---
+++ lintu/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lintu
+//
+//  Created by Frank Prößdorf on 1/24/13.
+//  Copyright (c) 2013 Frank Prößdorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lintu/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lintu
+//
+//  Created by Frank Prößdorf on 1/24/13.
+//  Copyright (c) 2013 Frank Prößdorf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
